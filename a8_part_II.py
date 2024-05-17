8dcf12cd4bea5cd78dd2a94a0ffeea183900fd88
Omar Figueroa <ofigueroa8@cps.edu>
2024-05-17 10:39:30 -0500

    Completed Get Planet Radius

--- a8_part_II.py
+++ a8_part_II.py
@@ -19,8 +19,9 @@ def get_planet_radius(planet_name: str) -> str:
         radius of the given planet
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(planet_name)))
+    # print(infobox_text)
     # TODO: fill this in
-    pattern = "REPLACE ME"
+    pattern = "Polar radius\n*(?P<radius>[\d.,]+)"
     error_text = "Page infobox has no polar radius information"
     match = get_match(infobox_text, pattern, error_text)
     return match.group("radius")
@@ -36,43 +37,50 @@ def get_birth_date(name: str) -> str:
         birth date of the given person
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
+    print(infobox_text)
     # TODO: fill this in
-    pattern = "REPLACE ME"
+    pattern = "Born\S+\s\S+\s\S+\((?P<birth>([\d-]+))"
     error_text = (
         "Page infobox has no birth information (at least none in xxxx-xx-xx format)"
     )
     match = get_match(infobox_text, pattern, error_text)
+    # print(match.groups())
     return match.group("birth")
 
 
 if __name__ == "__main__":
     print("\n<<<<<<<<<<<<<< Testing Planet Radius >>>>>>>>>>>>>>")
-    # should be 3,376.2
-    print(f'Mars has a polar radius of {get_planet_radius("Mars")}km')
-    # should be 6356.752
-    print(f'Earth has a polar radius of {get_planet_radius("Earth")}km')
-    # should be 66,854
-    print(f'Jupiter has a polar radius of {get_planet_radius("Jupiter")}km')
-    # should be 54,364
-    print(f'Saturn has a polar radius of {get_planet_radius("Saturn")}km')
+    # # should be 3,376.2
+    # print(f'Mars has a polar radius of {get_planet_radius("Mars")}km')
+    # # should be 6356.752
+    # print(f'Earth has a polar radius of {get_planet_radius("Earth")}km')
+    # # should be 66,854
+    # print(f'Jupiter has a polar radius of {get_planet_radius("Jupiter")}km')
+    # # should be 54,364
+    # # infobox_text = clean_text(get_first_infobox_text(get_page_html("Saturn")))
+    # # print(infobox_text)
+    # print(f'Saturn has a polar radius of {get_planet_radius("Saturn")}km')
 
     # uncomment below lines for tests once you think you're getting the right output
-    # print('\n<<<< Running asserts, this might take a sec >>>>')
-    # assert get_planet_radius("Mars") == "3376.2", "Incorrect radius for Mars"
-    # assert get_planet_radius("Earth") == "6356.752", "Incorrect radius for Earth"
-    # assert get_planet_radius("Jupiter") == "66,854", "Incorrect radius for Jupiter"
-    # assert get_planet_radius("Saturn") == "54,364", "Incorrect radius for Saturn"
-    # print('\n<<<< Planet radius tests passed >>>>')
+    print('\n<<<< Running asserts, this might take a sec >>>>')
+    assert get_planet_radius("Mars") == "3376.2", "Incorrect radius for Mars"
+    assert get_planet_radius("Earth") == "6356.752", "Incorrect radius for Earth"
+    assert get_planet_radius("Jupiter") == "66,854", "Incorrect radius for Jupiter"
+    assert get_planet_radius("Saturn") == "54,364", "Incorrect radius for Saturn"
+    print('\n<<<< Planet radius tests passed >>>>')
+
 
     print("\n<<<<<<<<<<<<<< Testing Birth Dates >>>>>>>>>>>>>>")
+    # infobox_text = clean_text(get_first_infobox_text(get_page_html("Alan Turing")))
+    # print(infobox_text)
     # should be 1906-12-09
-    print(format_birth(get_birth_date("Grace Hopper"), "Grace Hopper"))
-    # should be 1912-06-23
-    print(format_birth(get_birth_date("Alan Turing"), "Alan Turing"))
-    # should be 1955-06-08
-    print(format_birth(get_birth_date("Tim Berners-Lee"), "Tim Berners-Lee"))
-    # should be 1949-01-17
-    print(format_birth(get_birth_date("Anita Borg"), "Anita Borg"))
+    # print(format_birth(get_birth_date("Grace Hopper"), "Grace Hopper"))
+    # # should be 1912-06-23
+    # print(format_birth(get_birth_date("Alan Turing"), "Alan Turing"))
+    # # should be 1955-06-08
+    # print(format_birth(get_birth_date("Tim Berners-Lee"), "Tim Berners-Lee"))
+    # # should be 1949-01-17
+    # print(format_birth(get_birth_date("Anita Borg"), "Anita Borg"))
 
     # uncomment below lines for tests once you think you're getting the right output
     # print('\n<<<< Running asserts, this might take a sec >>>>')
